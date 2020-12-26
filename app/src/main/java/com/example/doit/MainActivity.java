fb8fcd10d2106186b74497150a82536ca8da7932
Vipal <vipallad2497@gmail.com>
2020-12-26 16:55:05 +0530

    Created ToDoAdapter, ToDoModel related tasks and new Tasks xml along with splash activity.

--- app/src/main/java/com/example/doit/MainActivity.java
+++ app/src/main/java/com/example/doit/MainActivity.java
@@ -1,14 +1,47 @@
 package com.example.doit;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.example.doit.Adapter.ToDoAdapter;
+import com.example.doit.Model.ToDoModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView tasksRecyclerView;
+    private ToDoAdapter tasksAdapter;
+
+    private List<ToDoModel> taskList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide(); //hides topmost actionbar
+
+        taskList = new ArrayList<>();
+        tasksRecyclerView = findViewById(R.id.tasksRecyclerview);
+        tasksRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        tasksAdapter = new ToDoAdapter(this);
+        tasksRecyclerView.setAdapter(tasksAdapter);
+
+        ToDoModel task = new ToDoModel();
+        task.setTask("This is a TestTask");
+        task.setStatus(0);
+        task.setId(1);
+
+        taskList.add(task);
+        taskList.add(task);
+        taskList.add(task);
+        taskList.add(task);
+        taskList.add(task);
+
+        tasksAdapter.setTasks(taskList);
     }
 }
